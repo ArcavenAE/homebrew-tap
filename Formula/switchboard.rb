bed7451e5626b086add2bd12c1e3cc821fbe9eeb
Michael Pursifull <mike@arcaven.com>
2026-03-08 13:11:31 -0500

    Fix formula style: desc, arch detection, redundant shell_output arg
    - desc must not start with formula name, must be capitalized
    - Replace on_arm/on_intel blocks with if/else (strict audit compat)
    - Remove redundant 0 exit status from shell_output in tests

--- Formula/switchboard.rb
+++ Formula/switchboard.rb
@@ -1,15 +1,13 @@
 class Switchboard < Formula
-  desc "switchboard"
+  desc "Low-latency encrypted tmux session router"
   homepage "https://github.com/arcaven/switchboard"
   version "0.1.0-alpha.20260307.672eadf"
   license "MIT"
 
-  on_arm do
+  if Hardware::CPU.arm?
     url "https://github.com/arcaven/switchboard/releases/download/alpha-20260307-672eadf/switchboard-darwin-arm64"
     sha256 "ffdd95a275d739250cc8d1adbb5eccb3f10890c6dad853f90c773272532a1fc6"
-  end
-
-  on_intel do
+  else
     url "https://github.com/arcaven/switchboard/releases/download/alpha-20260307-672eadf/switchboard-darwin-amd64"
     sha256 "c32dec6af95def543593dfc77cd3e2102fc61f74feb99a104d84d26da6c7598f"
   end
@@ -20,6 +18,6 @@ class Switchboard < Formula
   end
 
   test do
-    assert_match "switchboard", shell_output("#{bin}/switchboard --version 2>&1", 0)
+    assert_match "switchboard", shell_output("#{bin}/switchboard --version 2>&1")
   end
 end
--- Formula/threedoors.rb
+++ Formula/threedoors.rb
@@ -1,15 +1,13 @@
 class Threedoors < Formula
-  desc "Three Doors - radical task management showing only 3 tasks at a time"
+  desc "Radical task management showing only 3 tasks at a time"
   homepage "https://github.com/arcaven/ThreeDoors"
   version "0.1.0-alpha.20260308.90c79bf"
   license "MIT"
 
-  on_arm do
+  if Hardware::CPU.arm?
     url "https://github.com/arcaven/ThreeDoors/releases/download/alpha-20260308-90c79bf/threedoors-darwin-arm64"
     sha256 "b933fb3278b8614b7c0fd1dfece1afe21470dc2e17a1a467a0d7a666f5f30a45"
-  end
-
-  on_intel do
+  else
     url "https://github.com/arcaven/ThreeDoors/releases/download/alpha-20260308-90c79bf/threedoors-darwin-amd64"
     sha256 "786dee95896ab4f581e0bd731a92f3bb99f2f8ad050626a4e173c85b5b9bea2f"
   end
@@ -20,6 +18,6 @@ class Threedoors < Formula
   end
 
   test do
-    assert_match "ThreeDoors", shell_output("#{bin}/threedoors --version 2>&1", 0)
+    assert_match "ThreeDoors", shell_output("#{bin}/threedoors --version 2>&1")
   end
 end
